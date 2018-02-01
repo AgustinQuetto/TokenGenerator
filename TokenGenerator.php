eab7e25196940bdd81f806d4b6e73f63f47cfc7e
Agustín Quetto <agustinquetto@gmail.com>
2018-02-01 03:35:30 -0300

    Update TokenGenerator.php

--- TokenGenerator.php
+++ TokenGenerator.php
@@ -3,8 +3,8 @@
 //Función que genera el token para sincronización
 function generateToken($length = 8) {
 
-return substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, $length);
+return substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, $length); //retorna el token
 
 }
 
-echo generateToken(); //devuelve el token
+echo generateToken(); //imprime el token
